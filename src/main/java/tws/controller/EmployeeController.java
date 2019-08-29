5f662f4c62eab01570c797a843bc1594d3137a3c
xiongwei <2894001817@qq.com>
2019-08-29 15:34:07 +0800

    完成post和get方法

--- src/main/java/tws/controller/EmployeeController.java
+++ src/main/java/tws/controller/EmployeeController.java
@@ -2,11 +2,7 @@ package tws.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tws.entity.Employee;
 import tws.repository.EmployeeMapper;
 
@@ -25,5 +21,16 @@ public class EmployeeController {
     public ResponseEntity<List<Employee>> getAll() {
         return ResponseEntity.ok(employeeMapper.selectAll());
     }
-
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployees(@PathVariable String id) {
+        Employee employee = employeeMapper.selectById(id);
+        return ResponseEntity.ok(employee);
+    }
+    @PostMapping()
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        String id = UUID.randomUUID().toString();
+        employee.setId(id);
+        employeeMapper.addEmployee(employee);
+        return ResponseEntity.created(URI.create("/employees"+id)).build();
+    }
 }
--- src/main/java/tws/repository/EmployeeMapper.java
+++ src/main/java/tws/repository/EmployeeMapper.java
@@ -1,6 +1,7 @@
 package tws.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import tws.entity.Employee;
 
 import java.util.List;
@@ -8,4 +9,8 @@ import java.util.List;
 @Mapper
 public interface EmployeeMapper {
     List<Employee> selectAll();
+
+    void addEmployee(@Param("employee") Employee employee );
+
+    Employee selectById(@Param("id") String id);
 }
